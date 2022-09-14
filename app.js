01ce80dcfbd3c8d38618ba17422bb2f0fccc8668
SoniSuciadi <sonisuciadi@gmail.com>
2022-09-14 14:17:35 +0700

    done

--- app.js
+++ app.js
@@ -32,4 +32,4 @@ io.on("connection", (socket) => {
 io.on("connection", (socket) => {
   // console.log(socket.id); // x8WIv7-mJelg7on_ALbx
 });
-server.listen(3000);
+server.listen(port);
--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,13 @@
 const mongoose = require("mongoose");
+
 const uri =
+  process.env.MONGO_URI ||
   "mongodb+srv://sonisuciadi:03082000@cluster0.eevav4f.mongodb.net/DesignInDonkDb";
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log(`${uri} terkoneksi...`);
 });
